95912a83a223a8d5abad0cd59e709093bfa29160
Lewis Jones Ndubi <lamarjonesndubi@gmail.com>
2024-07-03 00:42:03 +0300

    adds index.js

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// index.js <by: Lamar Jones Ndubi>
 
-// Write your code here
+const num1 = 8;
+const num2 = 7.75;
+const multiply = num1 * num2;
+console.log(multiply); 
+
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random); 
+
+const num3 = 20;
+const num4 = 16; 
+const mod = num3 % num4;
+console.log(mod); 
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
+console.log(max); 
